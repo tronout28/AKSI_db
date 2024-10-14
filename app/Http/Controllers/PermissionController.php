f00867dd746159b5f984349e6bc7cc154a787b50
tronout28 <fatihabdurahman28@gmail.com>
2024-10-14 10:29:44 +0700

    [FA] feat checked if absence cant sick and permission

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Permission;
+use App\Models\Attendance;
+use Carbon\Carbon;
 
 class PermissionController extends Controller
 {
@@ -53,6 +55,19 @@ class PermissionController extends Controller
             'reason' => 'required|string',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
         ]);
+
+        $user = auth()->user();
+        $today = Carbon::today('Asia/Jakarta');
+
+        // Cek apakah sudah absen hari ini
+        $attendance = Attendance::where('user_id', $user->id)->whereDate('check_in_time', $today)->first();
+
+        if ($attendance) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Anda sudah absen hari ini, tidak bisa mengajukan izin.',
+            ], 403);
+        }
 
         $imageName = null;
 
